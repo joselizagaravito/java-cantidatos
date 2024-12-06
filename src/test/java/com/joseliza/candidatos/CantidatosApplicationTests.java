467474c028bb0d050e1999fa56b5a275d5d8041f
jose liza <joselizagaravito@gmail.com>
2024-12-06 14:08:17 -0500

    Inicio de desarrollo

---
+++ src/test/java/com/joseliza/candidatos/CantidatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseliza.candidatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantidatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
